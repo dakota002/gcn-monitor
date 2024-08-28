bb89870ec2bd16f2c4deddf25b7666f4105da413
dakota002 <dakota.d@live.com>
2024-08-28 13:35:19 -0400

    updates the consumer loop to put consumed topics in the s3 bucket

--- gcn_monitor/kafka.py
+++ gcn_monitor/kafka.py
@@ -9,12 +9,21 @@
 
 import json
 import logging
+from datetime import datetime, timezone
 
+import boto3
 import gcn_kafka
 
 from . import metrics
 
 log = logging.getLogger(__name__)
+s3_client = boto3.client("s3")
+resource_client = boto3.client("resourcegroupstaggingapi")
+
+BUCKET_NAME = bucket = resource_client.get_resources(
+    TagFilters=[{"Key": "aws:cloudformation:logical-id", "Values": ["NoticesBucket"]}],
+    ResourceTypeFilters=["s3"],
+)["ResourceTagMappingList"][0]["ResourceARN"].replace("arn:aws:s3:::", "")
 
 
 def stats_cb(data):
@@ -44,3 +53,5 @@ def run():
                 log.info("topic %s: got message", topic)
                 partition = message.partition()
                 metrics.received.labels(topic, partition).inc()
+                key = f"{topic}-{datetime.now(timezone.utc)}"
+                s3_client.put_object(Bucket=BUCKET_NAME, Key=key, Body=message.value())
